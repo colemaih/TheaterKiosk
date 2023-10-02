9c8d0101ee8d578a657ecab6c96e7275c051f87a
colemaih <colemaih@mail.uc.edu>
2023-10-01 21:42:54 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter your age: ");
+        int age = input.nextInt();
+        if (age >= 21) {
+            System.out.println("You get a wrist band!");
+        }
+    }
+}
